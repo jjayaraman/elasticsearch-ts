8476b98234da7d1b9bab7d650992e88e202cd678
Jayakumar Jayaraman <india.jai@gmail.com>
2022-06-18 09:28:59 +0100

    elastic search ts added

---
+++ search/out/hello.js
@@ -0,0 +1,5 @@
+"use strict";
+const hello = (name) => {
+    console.log(`Hello ${name}`);
+};
+hello('jay');
---
+++ search/src/elasticsearch.ts
@@ -0,0 +1,30 @@
+import { Client } from "@elastic/elasticsearch";
+import { CreateRequest, CreateResponse, SearchRequest, SearchResponse } from "@elastic/elasticsearch/lib/api/types";
+import { v4 as uuidv4 } from 'uuid';
+
+class ElasticsearchUtils {
+
+    constructor(private client: Client) { }
+
+
+    create = (index: string, document: string): Promise<CreateResponse> => {
+        const createRequest: CreateRequest = {
+            id: uuidv4(),
+            index,
+            document
+        }
+        return this.client.create(createRequest)
+    }
+
+
+    search = async (index: string, query: Object): Promise<SearchResponse> => {
+        const searchRequest: SearchRequest = {
+            index,
+            query
+        }
+
+        const result = await this.client.search(searchRequest)
+        return result
+    }
+
+}
